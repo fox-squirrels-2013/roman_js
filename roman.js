99702ccf10de84ec6641ba460a9e3f0235e0d353
raorao <savinirs@gmail.com>
2013-11-05 10:29:34 -0800

    meh

--- roman.js
+++ roman.js
@@ -1,1 +1,22 @@
 // Write your roman numeral code here
+var r_array;
+function roman(x) 
+{
+  r_array = r_array || [];
+  for(var i = 0; i < x; i++)
+  {  
+    if (i < 4) {
+      r_array.push("I");
+    } else {
+      r_array = []
+      r_array.push("V");
+      x %= 5;
+      roman(x);
+    }
+  }
+  var romanNumber = r_array.reverse().join("");
+  console.log(romanNumber);
+
+  return romanNumber;
+
+}
--- roman_spec.js
+++ roman_spec.js
@@ -5,3 +5,7 @@ function assert(outcome,description){
 // Write your TDD code here
 
 assert(roman(1)=="I", "it converts 1 to I")
+assert(roman(2)=="II", "it converts 2 to II")
+assert(roman(3)=="III", "it converts 3 to III")
+assert(roman(5)=="V", "it converts 5 to V")
+assert(roman(6)=="VI", "it converts 6 to VI")
