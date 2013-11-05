76f23b67a795dc8af38c18f918dc8018695b5004
Apprentice <apprentice@dbc07.local>
2013-11-05 09:52:49 -0800

    Roman Numerals from 1 to 100

--- roman.js
+++ roman.js
@@ -1,1 +1,51 @@
 // Write your roman numeral code here
+
+function roman(number){
+  var numeral=""
+
+  if (number >= 100)
+  {
+    while (number >= 100)
+    {
+      numeral+="C"
+      number-=100
+    }
+  }
+  
+  if (number >= 50)
+  {
+    while (number >= 50)
+    {
+      numeral+="L";
+      number-=50;
+    }
+  }
+
+  if (number >= 10)
+  {
+    while (number >= 10)
+    {
+      numeral+="X";
+      number-=10;
+    }
+  }
+
+  if (number >= 5)
+  {
+    while (number >= 5)
+    {
+      numeral+="V";
+      number-=5;
+    }
+  }
+
+  if (number >= 1)
+    {
+      while (number > 0) 
+      {
+        numeral+="I";
+        number--;
+      }
+    }
+  return numeral;
+}
--- roman_spec.js
+++ roman_spec.js
@@ -5,3 +5,11 @@ function assert(outcome,description){
 // Write your TDD code here
 
 assert(roman(1)=="I", "it converts 1 to I")
+assert(roman(2)=="II", "it converts 2 to II")
+assert(roman(5)=="V", "it converts 5 to V")
+assert(roman(6)=="VI", "it converts 6 to VI")
+assert(roman(10)=="X", "it converts 10 to X")
+assert(roman(35)=="XXXV", "it converts 35 to XXXV")
+assert(roman(50)=="L", "it converts 50 to L")
+assert(roman(75)=="LXXV", "it converts 75 to LXXV")
+assert(roman(100)=="C", "it converts 100 to C")
