8cd5ffe701695e67b4b5f3832d543caf93f09517
Apprentice <apprentice@dbc14.local>
2013-11-05 10:29:10 -0800

    working if/self, not working function

--- roman.js
+++ roman.js
@@ -1,1 +1,64 @@
 // Write your roman numeral code here
+
+var roman = function(num) {
+  var result = "";
+  var romanHash = {
+    100: "C",
+    50: "L",
+    5: "V",
+    10: "X",
+    1: "I"
+  };
+  var romanArray = [];
+  for (var key in romanHash) {romanArray.push([key, romanHash[key]])}; 
+    romanArray = romanArray.reverse();
+  while (num > 0){
+    for (var index in romanArray){
+      if (Math.floor(num/index) == 1){
+        result += romanArray[index][1];
+      num -= romanArray[index][0];
+      }
+    }
+  };
+  return result;
+};
+
+
+// for(var index in items) {
+//   document.write( index + " : " + items[index] + "<br />");
+// }
+
+
+    // if (num == 100) {
+    //   result += "C";
+    //   num -= 100;
+    // }
+    // else if (num >= 50) {
+    //   result += "L";
+    //   num -= 50;
+    // }
+    // else if (num >= 10) {
+    //   result += "X";
+    //   num -= 10;
+    // }
+    // else if (num >= 5) {
+    //   result += "V";
+    //   num -= 5;
+    // }
+    // else {
+    //   result += "I";
+    //   num -= 1;
+    // }
+
+
+
+  // if (num == 1) {
+  //   return "I";
+  // } 
+  // else if (num == 2) {
+  //   return "II";
+  // }
+  // else if {
+  //   return "V";
+  // }
+  // else 
--- roman_spec.js
+++ roman_spec.js
@@ -5,3 +5,12 @@ function assert(outcome,description){
 // Write your TDD code here
 
 assert(roman(1)=="I", "it converts 1 to I")
+assert(roman(2)=="II", "it converts 2 to II")
+assert(roman(5)=="V", "it converts 5 to V")
+assert(roman(6)=="VI", "it converts 6 to VI")
+assert(roman(7)=="VII", "it converts 7 to VII")
+assert(roman(10)=="X", "it converts 10 to X")
+assert(roman(59)=="LVIIII", "it converts 59 to LVIIII")
+assert(roman(99)=="LXXXXVIIII", "it converts 99 to LXXXXVIIII")
+assert(roman(100)=="C", "it converts 100 to C")
+
