a7ccae6e51e8f2c2796bd6f29ffdc9908f1d513d
Apprentice <apprentice@dbc16.local>
2013-11-05 10:28:33 -0800

    first commit... tdd tests failing at 9

--- roman.js
+++ roman.js
@@ -1,1 +1,21 @@
-// Write your roman numeral code here
+function roman(digi) {
+  var answer = "";
+  if (digi >= 5 ) {
+    answer = answer + "V";
+    digi = digi - 5;
+  };
+  if (digi == 1) {
+    answer = answer + "I";
+    return answer;
+  } else if (digi ==2) {
+    answer = answer + "II"
+    return answer;
+  } else if (digi == 3) {
+    answer = answer + "III"
+    return answer;
+  } else if (digi == 4) {
+    return "IV";
+  } else {
+    return answer;
+  }
+}
--- roman_spec.js
+++ roman_spec.js
@@ -4,4 +4,11 @@ function assert(outcome,description){
 
 // Write your TDD code here
 
-assert(roman(1)=="I", "it converts 1 to I")
+assert(roman(1)=="I", "it converts 1 to I");
+assert(roman(2)=="II", "it converts 2 to II");
+assert(roman(3)=="III", "it converts 3 to III");
+assert(roman(4)=="IV", "it converts 4 to IV");
+assert(roman(5)=="V", "it converts 5 to V");
+assert(roman(9)=="IX", "it converts 9 to IX");
+
+
