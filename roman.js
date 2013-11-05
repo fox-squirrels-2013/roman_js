e9cb72f9fe95168a913f957dcda691ff04d2f978
Apprentice <apprentice@dbc-11.local>
2013-11-05 10:30:14 -0800

    beni and marc roman numerals

--- roman.js
+++ roman.js
@@ -1,1 +1,26 @@
 // Write your roman numeral code here
+function roman(x) {
+
+  var roman_string = ''
+
+  if (x > 5) {
+    roman_string + 'V';
+    x - 5
+  }
+  else if (x === 1){
+    roman_string + 'I';
+  }
+  return roman_string
+} ;
+
+ // else if (x === 2 )
+  // {
+  //    return "II";
+  // }
+  // else if (x === 4)
+  // {
+  //   return "IV";
+  // }
+  // else if (x === 5)
+  //   return "V";
+
--- roman_spec.js
+++ roman_spec.js
@@ -2,6 +2,9 @@ function assert(outcome,description){
   console.log(outcome + " : " + description)
 }
 
-// Write your TDD code here
 
-assert(roman(1)=="I", "it converts 1 to I")
+assert(roman(1)==="I", "it converts 1 to I")
+assert(roman(2)==="II", "it converts 2 to II")
+assert(roman(4)==="IV", "it converts 4 to IV")
+assert(roman(5)==="V", "it converts 5 to V")
+assert(roman(6)==="VI", "it converts 6 to VI")
