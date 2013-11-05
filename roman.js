60a9cc5be4ffdc5254ecf4313af52c72545599d6
Apprentice <apprentice@dbc13.local>
2013-11-05 10:08:29 -0800

    roman working to 50 no special cases

--- roman.js
+++ roman.js
@@ -1,1 +1,29 @@
 // Write your roman numeral code here
+
+
+function roman(arabic){
+  var string = ''
+
+
+  if (arabic < 50 && arabic >= 10) {
+    for(var i = 0; i < Math.floor(arabic / 10); i++) {
+      string += 'X';
+    }
+    arabic %= 10;
+  }  
+
+  if (arabic < 10 && arabic >= 5) {
+    for(var i = 0; i < Math.floor(arabic / 5); i++) {
+      string += 'V';
+    }
+    arabic %= 5;
+  }
+
+  if (arabic < 5) {
+    for(var i = 0; i < arabic; i++) {
+      string += 'I';
+    }
+  }
+
+  return string
+};
--- roman_spec.js
+++ roman_spec.js
@@ -5,3 +5,14 @@ function assert(outcome,description){
 // Write your TDD code here
 
 assert(roman(1)=="I", "it converts 1 to I")
+assert(roman(2)=="II", "it converts 2 to II")
+assert(roman(3)=="III", "it converts 3 to III")
+assert(roman(5)=="V", "it converts 5 to V")
+assert(roman(6)=="VI", "it converts 6 to VI")
+assert(roman(8)=="VIII", "it converts 8 to VIII")
+assert(roman(10)=="X", "it converts 10 to X")
+assert(roman(13)=="XIII", "it converts 13 to XIII")
+assert(roman(23)=="XXIII", "it converts 23 to XXIII")
+assert(roman(40)=="XXXX", "it converts 40 to XXXX")
+assert(roman(45)=="XXXXV", "it converts 45 to XXXXV")
+
