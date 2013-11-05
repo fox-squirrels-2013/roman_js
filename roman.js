18f1f3b50624b49c3fcbc89b532b7f99ffd6f3f3
raorao <savinirs@gmail.com>
2013-11-05 12:31:33 -0800

    take that javascript, you don't know me

--- roman.js
+++ roman.js
@@ -1,22 +1,73 @@
 // Write your roman numeral code here
-var r_array;
 function roman(x) 
 {
-  r_array = r_array || [];
-  for(var i = 0; i < x; i++)
-  {  
-    if (i < 4) {
-      r_array.push("I");
+  var romanArray = [];
+  for (var i = 0; x > i; i++) 
+  {
+    if (x >= 10) {
+      romanArray.push("X");
+      x -= 9;
+    } else if (x >= 5) {
+      romanArray.push("V");
+      x -= 4;
     } else {
-      r_array = []
-      r_array.push("V");
-      x %= 5;
-      roman(x);
+      romanArray.push("I");
     }
+  
   }
-  var romanNumber = r_array.reverse().join("");
-  console.log(romanNumber);
-
-  return romanNumber;
-
+    var romanNumber = romanArray.join("");
+    console.log(romanNumber);
+    return romanNumber;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // r_array = r_array || [];
+  // for(var i = 0; i < x; i++)
+  // {  
+  //   if (i < 4) {
+  //     r_array.push("I");
+  //   } else {
+  //     r_array = []
+  //     r_array.push("V");
+  //     x %= 5;
+  //     roman(x);
+  //   }
+  // }
+  // var romanNumber = r_array.reverse().join("");
+  // console.log(romanNumber);
+
+  // return romanNumber;
+
